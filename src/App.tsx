260acd6826fc800791915794840a27ea0a9751a6
Gabriel <souzzagabe@hotmail.com>
2024-01-12 19:06:23 -0300

    ajustes

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        ready to code
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+import { extendTheme } from "@chakra-ui/react";
+
+// include custom option in here, like collors, fonts ..
+
+const customTheme = {};
+
+const theme = extendTheme(customTheme);
+
+export default theme;
